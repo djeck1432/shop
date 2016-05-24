ddaf93fc47850e2b608d3a87cd1efc48840760bf
Oleksiy <doubledare704@gmail.com>
2016-05-24 19:31:38 +0300

    Added translations

--- futbolkas/main/models.py
+++ futbolkas/main/models.py
@@ -1,38 +1,29 @@
 from django.db import models
 
 
-
-
-
 class Category(models.Model):
-	name =models.CharField(max_length= 255 , verbose_name= "Категория ")
-	alias =models.SlugField(verbose_name= "alias  категории")
+    name = models.CharField(max_length=255, verbose_name="Категория ")
+    alias = models.SlugField(verbose_name="alias  категории")
 
-	class Meta():
-		verbose_name="Категория "
-		verbose_name_plural= "Категории"
-	def __str__(self):
-		return "Категория %s" % self.name
+    class Meta:
+        verbose_name = "Категория "
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return "Категория %s" % self.name
 
 
 class Item(models.Model):
-	name =models.CharField(max_length= 255 , verbose_name= "Названия товара")
-	price =models.IntegerField(default =0 , verbose_name= "Цена")
-	image =models.CharField(max_length= 255 , verbose_name="Фото")
-	alias =models.SlugField(verbose_name= "alias  категория")
+    name = models.CharField(max_length=255, verbose_name="Названия товара")
+    price = models.IntegerField(default=0, verbose_name="Цена")
+    image = models.CharField(max_length=255, verbose_name="Фото")
+    alias = models.SlugField(verbose_name="alias  категория")
 
+    category = models.ForeignKey(Category)
 
+    class Meta:
+        verbose_name = "Товар "
+        verbose_name_plural = "Товары"
 
-
-	category= models.ForeignKey(Category)
-
-	class Meta():
-		verbose_name="Товар "
-		verbose_name_plural= "Товары"
-	def __str__(self):
-		return "Товар %s" % self.name
-
-#-----------------------------------------------------------------------
-
-#-----------------------------------------------------------------------
-
+    def __str__(self):
+        return "Товар %s" % self.name
