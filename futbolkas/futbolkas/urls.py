d27098a1b33795f062d4ae8297d861eab3d2bca2
Oleksiy <doubledare704@gmail.com>
2016-06-01 11:45:11 +0300

    last push
    last push

--- futbolkas/futbolkas/urls.py
+++ futbolkas/futbolkas/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, patterns, include
 from django.contrib import admin
 from django.views.i18n import set_language
 from django.views.decorators.csrf import csrf_exempt
+from django.conf.urls.static import static
 
-from futbolkas.settings import DEBUG, MEDIA_ROOT
 from main.views import PaysCreateView
 
 urlpatterns = patterns('',
@@ -32,11 +33,4 @@ urlpatterns = patterns('',
                        url(r'^order/', 'main.views.order'),
                        url(r'^(?P<alias>[^/]+)', 'main.views.get_category'),
                        )
-if DEBUG:
-    # serve files from media folder
-    urlpatterns += patterns('',
-                            url(r'^media/(?P<path>.*)$',
-                                'django.views.static.serve',
-                                {'document_root': MEDIA_ROOT}),
-                            )
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
